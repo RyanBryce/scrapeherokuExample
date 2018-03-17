4e8ac17050f6ae3429cbae68457bb8d289c3575c
Ryan Bryce <ryan.bryce1992@gmail.com>
2018-03-16 18:22:02 -0600

    heroku port fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -29,7 +29,8 @@ app.use(express.static("public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/week18Populater"
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
